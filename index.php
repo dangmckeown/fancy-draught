3fec4541c931488eb21772afe9b66a4beadc3dd5
Dan McKeown <dangmckeown@users.noreply.github.com>
2016-01-25 14:04:08 +0000

    Create index.php
    First test commit to see if API is working

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+$dan = array(
+0=>"My team",
+1	=>	3,
+2	=>	323,	
+3	=>	449,
+4	=>	131,
+5	=>	471,
+6	=>	301,
+7	=>	398,
+8	=>	197,
+9	=>	217,
+10	=>	67,
+11	=>	87,
+12	=>	112,
+13	=>	371,
+14	=>	171,
+15	=>	250	
+
+);
+
+$url = "http://fantasy.premierleague.com/web/api/elements/" . $dan[2] . "/";
+$content = file_get_contents($url);
+$json = json_decode($content, true);
+
+echo $json[1]['total_score'];
+
+?>
