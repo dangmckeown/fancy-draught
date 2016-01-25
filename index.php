024447228ef7785174ba2cf0438a468215baf738
Dan McKeown <dangmckeown@users.noreply.github.com>
2016-01-25 15:06:52 +0000

    It works! Whack the other teams in.

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 function add_up($team){
+  echo "<h3>". $team[0] ."</h3>"
   echo "<p>";
   for($i = 1; $i<=15; $i++)
   {
@@ -39,6 +40,88 @@ $dan = array(
 "score" => 0
 );
 
+$holly = array(
+0=>"The Bengal Penguins",
+1	=>	465	,
+2	=>	259	,
+3	=>	132	,
+4	=>404	,
+5	=>	16	,
+6	=>	238	,
+7	=>	338	,
+8	=>	457	,
+9	=>	222	,
+10	=>	98	,
+11	=>	321	,
+12	=>	30	,
+13	=>	185	,
+14	=>	188	,
+15	=>	376,
+"score" => 0
+);
+
+$pete = array(
+0=>"Dr. Aftlove or: How I Learned to Stop Worrying and Love Saido Berahino as a Second Round Pick sponsored by Torque, it’s what moves boulders, Memorial XV",
+1	=>	205	,
+2	=>	307	,
+3	=>	130	,
+4	=>	502	,
+5	=>	114	,
+6	=>	407	,
+7	=>	198	,
+8	=>	13	,
+9	=>	462	,
+10	=>	531	,
+11	=>	517	,
+12	=>	375	,
+13	=>	62	,
+14	=>	525	,
+15	=>	81,
+"score" => 0
+);
+
+$christ = array(
+0=>"Cilla Black Memorial XV",
+1	=>	395	,
+2	=>	104	,
+3	=>	379	,
+4	=>	355	,
+5	=>	86	,
+6	=>	191	,
+7	=>	189	,
+8	=>	14	,
+9	=>	482	,
+10	=>	350	,
+11	=>	441	,
+12	=>	300	,
+13	=>	129	,
+14	=>	63	,
+15	=>	293,
+"score" => 0
+);
+
+$christ = array(
+0=>"Repay Me Depay Kauto Star Roddy Piper Memorial XV",
+1	=>	353	,
+2	=>	79	,
+3	=>	4	,
+4	=>	302	,
+5	=>	532	,
+6	=>	216	,
+7	=>	341	,
+8	=>	416	,
+9	=>	521	,
+10	=>	51	,
+11	=>	149	,
+12	=>	275	,
+13	=>	529	,
+14	=>	74	,
+15	=>	240	,
+"score" => 0
+);
+
 add_up($dan);
+
+add_up($holly);
 
 ?>
