da0970c40b33dadf670b93f35f8580b78e59b1d0
Dan McKeown <dangmckeown@users.noreply.github.com>
2016-01-26 11:51:11 +0000

    Create teams.php
    All managers' teams with player refs for fantasy.premierleague json

---
+++ teams.php
@@ -0,0 +1,107 @@
+<?php
+
+$dan = array(
+"manager" => "Dan";
+"name"=>"Johnny-come-lately Ziggy Stardust/Hans Gruber Memorial XV",
+1	=>	3,
+2	=>	323,	
+3	=>	449,
+4	=>	131,
+5	=>	471,
+6	=>	301,
+7	=>	398,
+8	=>	197,
+9	=>	217,
+10	=>	67,
+11	=>	87,
+12	=>	112,
+13	=>	371,
+14	=>	171,
+15	=>	250,	
+"score" => 0
+);
+$holly = array(
+"manager" => "Holly";
+"name"=>"The Bengal Penguins",
+1	=>	465	,
+2	=>	259	,
+3	=>	132	,
+4	=>404	,
+5	=>	16	,
+6	=>	238	,
+7	=>	338	,
+8	=>	457	,
+9	=>	222	,
+10	=>	98	,
+11	=>	321	,
+12	=>	30	,
+13	=>	185	,
+14	=>	188	,
+15	=>	376,
+"score" => 0
+);
+$pete = array(
+"manager" => "Pete";
+"name"=>"Dr. Aftlove or: How I Learned to Stop Worrying and Love Saido Berahino as a Second Round Pick sponsored by Torque, it’s what moves boulders, Memorial XV",
+1	=>	205	,
+2	=>	307	,
+3	=>	130	,
+4	=>	502	,
+5	=>	114	,
+6	=>	407	,
+7	=>	198	,
+8	=>	13	,
+9	=>	462	,
+10	=>	531	,
+11	=>	517	,
+12	=>	375	,
+13	=>	62	,
+14	=>	525	,
+15	=>	81,
+"score" => 0
+);
+
+$christ = array(
+"manager" => "Christian";
+"name"=>"Cilla Black Memorial XV",
+1	=>	395	,
+2	=>	104	,
+3	=>	379	,
+4	=>	355	,
+5	=>	86	,
+6	=>	191	,
+7	=>	189	,
+8	=>	14	,
+9	=>	482	,
+10	=>	350	,
+11	=>	441	,
+12	=>	300	,
+13	=>	129	,
+14	=>	63	,
+15	=>	293,
+"score" => 0
+);
+
+
+$sam = array(
+"manager" => "Sam";
+"name"=>"Repay Me Depay Kauto Star Roddy Piper Memorial XV",
+1	=>	353	,
+2	=>	79	,
+3	=>	4	,
+4	=>	302	,
+5	=>	532	,
+6	=>	216	,
+7	=>	341	,
+8	=>	416	,
+9	=>	521	,
+10	=>	51	,
+11	=>	149	,
+12	=>	275	,
+13	=>	529	,
+14	=>	74	,
+15	=>	240	,
+"score" => 0
+);
+
+?>
