b0070eb6602e89e42f3eaf55f9cbbff2e2b8c3d9
Dan McKeown <dangmckeown@users.noreply.github.com>
2016-01-26 12:34:32 +0000

    Update index.php

--- index.php
+++ index.php
@@ -6,10 +6,10 @@ include_once('teams.php');
 
 //get those teams out there!
 auto_sort($holly);
-#auto_sort($pete);
-#auto_sort($christ);
-#auto_sort($sam);
-#auto_sort($dan);
+auto_sort($pete);
+auto_sort($christ);
+auto_sort($sam);
+auto_sort($dan);
 
 //end page
 include_once('footer.php');
