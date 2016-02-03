cfec22f32a1bc6be60b89e58dcc39e3c6cfb8c94
Dan McKeown <dangmckeown@users.noreply.github.com>
2016-02-03 09:44:38 +0000

    Roll back!

--- header.php
+++ header.php
@@ -1,6 +1,3 @@
 <?php
-echo "<!--header.php LOADED --><html><head><title>Fantasy draft 15-16</title>";
-include_once("styles.php");
-echo $style;
-echo "</head><body><h1>Draft teams</h1>";
+echo "<!--header.php LOADED --><html><head><title>Fantasy draft 15-16</title><style type='text/css'>td{border: 1px black solid; width:150px;vertical-align:top;}</style></head><body><h1>Draft teams</h1>";
 ?>
