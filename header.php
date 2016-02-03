e9ff3563420b553c90ef8a0187e42243535ecef7
Dan McKeown <dangmckeown@users.noreply.github.com>
2016-02-03 09:27:27 +0000

    Embed external stylesheet

--- header.php
+++ header.php
@@ -1,3 +1,3 @@
 <?php
-echo "<!--header.php LOADED --><html><head><title>Fantasy draft 15-16</title><style type='text/css'>td{border: 1px black solid; width:150px;vertical-align:top;}</style></head><body><h1>Draft teams</h1>";
+echo "<!--header.php LOADED --><html><head><title>Fantasy draft 15-16</title><link rel='stylesheet' type='text/css' src='fancy-draught.css' /></head><body><h1>Draft teams</h1>";
 ?>
