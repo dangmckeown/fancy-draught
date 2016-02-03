f5a8706b47364522da99bb8bec4723ecff4681aa
Dan McKeown <dangmckeown@users.noreply.github.com>
2016-02-03 09:37:16 +0000

    Update header.php

--- header.php
+++ header.php
@@ -1,3 +1,5 @@
 <?php
-echo "<!--header.php LOADED --><html><head><title>Fantasy draft 15-16</title><link rel='stylesheet' type='text/css' src='fancy-draught.css' /></head><body><h1>Draft teams</h1>";
+echo "<!--header.php LOADED --><html><head><title>Fantasy draft 15-16</title>";
+include_once("styles.php");
+echo "</head><body><h1>Draft teams</h1>";
 ?>
